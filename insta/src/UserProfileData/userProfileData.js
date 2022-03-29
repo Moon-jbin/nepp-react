8630ceff86bab10eceebea6773abec736cc8f6ff
Moon-jbin <84951837+Moon-jbin@users.noreply.github.com>
2022-03-29 14:13:59 +0900

    insta maincompo profile 퍼블리싱

---
+++ insta/src/UserProfileData/userProfileData.js
@@ -0,0 +1,38 @@
+export const  userProfileData = [
+  {
+    idx: "1",
+    img: "https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2",
+    id:"mun",
+    isFollow:"회원님을 팔로우 합니다.",
+    btn : "팔로우"
+  },
+  {
+    idx: "2",
+    img: "https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2",
+    id:"mjb1209",
+    isFollow:"  insta_hard 님 외 14명이 팔로우합니다.",
+    btn : "팔로우"
+  },
+  {
+    idx: "3",
+    img: "https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2",
+    id:"eajb5093",
+    isFollow:"회원님을 팔로우 합니다.",
+    btn : "팔로우"
+  },
+  {
+    idx: "4",
+    img: "https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2",
+    id:"aveq5430",
+    isFollow:"회원님을 팔로우 합니다.",
+    btn : "팔로우"
+  },
+  {
+    idx: "5",
+    img: "https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2",
+    id:"kwhbe1234",
+    isFollow:"회원님을 팔로우 합니다.",
+    btn : "팔로우"
+  }
+
+]
--- insta/src/components/pages/Main.jsx
+++ insta/src/components/pages/Main.jsx
@@ -1,5 +1,21 @@
 import styled from "styled-components";
+import { userProfileData } from "../../UserProfileData/userProfileData";
 const Main = () => {
+
+  const userProfileList = userProfileData.map((item)=>{
+    return(
+              <UserProfileWrap>
+                <UserProfileImg src={item.img}/>
+                <UserProfileInfo>
+                  <UserProfileId>{item.id}</UserProfileId>
+                  <IsFollow>{item.isFollow}</IsFollow>
+                </UserProfileInfo>
+                <FollowBtn>{item.btn}</FollowBtn>
+              </UserProfileWrap>
+    )
+  }
+  )
+
   return (
     <>
       <Container>
@@ -37,6 +53,28 @@ const Main = () => {
               <ProfileChange>전환</ProfileChange>
             </MyProfileWrap>
           </ProfileWrap>
+          <RecommendWrap>
+            <RecommendTop>
+              <RecommForUser>회원님을 위한 추천</RecommForUser>
+              <ViewAll>모두보기</ViewAll>
+            </RecommendTop>
+            <RecommendBottom>
+              <UserProfileWrap>
+                <UserProfileImg src=" https://mblogthumb-phinf.pstatic.net/20150427_261/ninevincent_1430122791768m7oO1_JPEG/kakao_1.jpg?type=w2" />
+                <UserProfileInfo>
+                  <UserProfileId>mun</UserProfileId>
+                  <IsFollow>회원님을 팔로우 합니다.</IsFollow>
+                </UserProfileInfo>
+                <FollowBtn>팔로우</FollowBtn>
+              </UserProfileWrap>
+              {userProfileList}
+            </RecommendBottom>
+          </RecommendWrap>
+          <CopyRightForm>
+            <CopyRight>
+               © 2022 INSTAGRAM FROM META
+            </CopyRight>
+          </CopyRightForm>
         </Right>
       </Container>
     </>
@@ -73,7 +111,7 @@ const Right = styled.div`
 const ProfileWrap = styled.div`
   width: 293px;
   height: 56px;
-  border: 1px solid #000;
+  /* border: 1px solid #000; */
 `;
 const MyProfileWrap = styled.div`
   display: flex;
@@ -90,10 +128,99 @@ const MyProfileImg = styled.img`
 const MyProfileInfo = styled.div`
   display: flex;
   flex-direction: column;
+  flex: 1;
+  padding: 0 10px;
+`;
+const MyProfileId = styled.span`
+  font-size: 13px;
+  font-weight: 600;
+`;
+const MyProfileName = styled.span`
+  font-size: 14px;
+  color: #8e8e8e;
+`;
+const ProfileChange = styled.button`
+  background: #fff;
+  border: 0;
+  font-size: 12px;
+  font-weight: 600;
+  color:#0095f6;
+`;
+
+// 회원님을 추천
+const RecommendWrap = styled.div`
+  margin: 24px 0 0 ;
+  width: 100%;
   border: 1px solid #000;
 `;
-const MyProfileId = styled.span``;
-const MyProfileName = styled.span``;
-const ProfileChange = styled.button``;
+const RecommendTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0 10px 0;
+`;
+const RecommForUser = styled.span`
+  font-size: 14px;
+  color: #8e8e8e;
+  font-weight: 700;
+`;
+const ViewAll = styled.span`
+  font-size: 13px;
+  color: #262626;
+  font-weight: 600;
+`;
+
+const RecommendBottom = styled.div`
+  width: 100%;
+`;
+const UserProfileWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 0 5px 0;
+`;
+const UserProfileImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+`;
+const UserProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 0 10px;
+`;
+const UserProfileId = styled.span`
+  font-size: 13px;
+  font-weight: 600;
+`;
+const IsFollow = styled.span`
+  width: 193px;
+  font-size: 12px;
+  font-weight: 500;
+  color: #8e8e8e;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+`;
+const FollowBtn = styled.button`
+  background: #fff;
+  border: 0;
+  font-size: 12px;
+  font-weight: 600;
+  color:#0095f6;
+`;
+
+// copyright
+const CopyRightForm = styled.div`
+  width: 100%;
+  padding: 20px 0 0;
+`;
+const CopyRight = styled.span`
+  font-size: 12px;
+  color: #c7c7c7;
+`;
+
 
 export default Main;
